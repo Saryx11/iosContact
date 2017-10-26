ba60a1921e98b21d1a7ab60e6c4e78bf1685f7e2
Unknown <louis.benjamin@live.fr>
2017-10-26 17:43:47 +0200

    get json from server

--- AddressBookSwift4/ContactsTableViewController.swift
+++ AddressBookSwift4/ContactsTableViewController.swift
@@ -11,86 +11,96 @@ import CoreData
 
 class ContactsTableViewController: UITableViewController {
     var persons = [Person]()
-
-    func reloadDataFromDataBase(){
-        let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
-        let sortFirstName = NSSortDescriptor(key: "firstName", ascending: true)
-        let sortLastName = NSSortDescriptor(key: "lastName", ascending: true)
-        fetchRequest.sortDescriptors = [sortFirstName, sortLastName]
-        let context = self.appDelegate().persistentContainer.viewContext
-        //print(try? context.fetch(fetchRequest))
-        guard let personsDB = try? context.fetch(fetchRequest) else{
-            return
-        }
-        persons=personsDB
-        self.tableView.reloadData()
-    }
     
+    /*func reloadDataFromDataBase(){
+     let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+     let sortFirstName = NSSortDescriptor(key: "firstName", ascending: true)
+     let sortLastName = NSSortDescriptor(key: "lastName", ascending: true)
+     fetchRequest.sortDescriptors = [sortFirstName, sortLastName]
+     let context = self.appDelegate().persistentContainer.viewContext
+     //print(try? context.fetch(fetchRequest))
+     guard let personsDB = try? context.fetch(fetchRequest) else{
+     return
+     }
+     persons=personsDB
+     //self.tableView.reloadData()
+     }*/
+    var resultController: NSFetchedResultsController<Person>!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         /* HOW TO USE PLIST FILES
          let namesPlist = Bundle.main.path(forResource: "names.plist", ofType: nil)
-        if let namesPath = namesPlist{
-            let url = URL(fileURLWithPath: namesPath)
-            let dataArray = NSArray(contentsOf: url)
-            
-            for dict in dataArray!{
-                if let dictionnary = dict as?[String: String]{
-                    let person = Person(firstName: dictionnary["lastname"]!, lastName: dictionnary["name"]!)
-                    persons.append(person)
-                    print(dictionnary)
-                }
-            }
-            print(dataArray)
-        }*/
+         if let namesPath = namesPlist{
+         let url = URL(fileURLWithPath: namesPath)
+         let dataArray = NSArray(contentsOf: url)
+         
+         for dict in dataArray!{
+         if let dictionnary = dict as?[String: String]{
+         let person = Person(firstName: dictionnary["lastname"]!, lastName: dictionnary["name"]!)
+         persons.append(person)
+         print(dictionnary)
+         }
+         }
+         print(dataArray)
+         }*/
         self.title = "Contacts"
-
+        let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+        let sortFirstName = NSSortDescriptor(key: "firstName", ascending: true)
+        let sortLastName = NSSortDescriptor(key: "lastName", ascending: true)
+        fetchRequest.sortDescriptors = [sortFirstName, sortLastName]
+        
+        resultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.appDelegate().persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        resultController.delegate = self
+        do{
+            try resultController.performFetch()
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    
+        
         let addContact = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addContactPress))
         self.navigationItem.rightBarButtonItem = addContact
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        reloadDataFromDataBase()
+        updateDataFromServer()
+        // reloadDataFromDataBase()
     }
-
+    
     @objc func addContactPress(){
         let addContactView = AddViewController(nibName: nil, bundle: nil)
         self.navigationController?.pushViewController(addContactView, animated: true)
         addContactView.delegate = self
-        //create and push addViewController
-        //set the delegate
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return persons.count
-    }
+    
+    /*override func numberOfSections(in tableView: UITableView) -> Int {
+     // #warning Incomplete implementation, return the number of sections
+     //return 1
+     }*/
+    
     /*override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath ) -> Int{
-        return 55
-    }*/
-
+     return 55
+     }*/
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailsViewController = DetailsViewController(nibName: nil, bundle: nil)
-        let person = persons[indexPath.row]
+        guard let person = self.resultController?.object(at: indexPath) else {
+            fatalError("Attempt to configure cell without a managed object")
+        }
         detailsViewController.person = person
         self.navigationController?.pushViewController(detailsViewController, animated: true)
         detailsViewController.delegate = self
@@ -98,62 +108,175 @@ class ContactsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsTableViewCell", for: indexPath)
-
+        
         if let contactCell = cell as? ContactsTableViewCell{
-            let person: Person = persons[indexPath.row]
+            guard let person = self.resultController?.object(at: indexPath) else {
+                fatalError("Attempt to configure cell without a managed object")
+            }
             contactCell.nameLabel.text = person.firstName! + " " + person.lastName!
         }
         // Configure the cell...
-
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func updateDataFromServer(){
+        let urlString = "http://192.168.116.2:3000/persons"
+        guard let requestUrl = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: requestUrl) {
+            (data, response, error) in
+            if error == nil,let usableData = data {
+                //JSONSerialization
+                let dictionary = try? JSONSerialization.jsonObject(with: usableData, options: JSONSerialization.ReadingOptions.mutableContainers)
+                guard let jsonDict = dictionary as? [[String: Any]]else{
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.updateFromJsonData(json: jsonDict)
+                    
+                    /*let context = self.appDelegate().persistentContainer.viewContext
+                     for personObject in jsonDict{
+                     let person = Person(entity: Person.entity(), insertInto: context)
+                     person.firstName = personObject["surname"] as? String ?? "DefaultName"
+                     person.lastName = personObject["lastname"] as? String ?? "DefaultName"
+                     }
+                     try? self.appDelegate().persistentContainer.viewContext.save()*/
+                }
+            }
+        }
+        task.resume()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func updateFromJsonData(json: [[String: Any]]){
+        let sort = NSSortDescriptor(key: "id", ascending: true)
+        let fetchrequest = NSFetchRequest<Person>(entityName: "Person")
+        fetchrequest.sortDescriptors = [sort]
+        
+        let context = self.appDelegate().persistentContainer.viewContext
+        let persons = try! context.fetch(fetchrequest)
+        let personIds = persons.map({(person) -> Int32 in
+            return person.id
+        })
+        let serversId = json.map{(dict)->Int in
+            return dict["id"] as? Int ?? 0
+        }
+        //delete if not on server
+        for person in persons{
+            if !serversId.contains(Int(person.id)){
+                context.delete(person)
+            }
+        }
+        //Update or create
+        for jsonPerson in json{
+            let id = jsonPerson["id"] as? Int ?? 0
+            if let index = personIds.index(of: Int32(id)){
+                persons[index].lastName = jsonPerson["lastname"] as? String ?? "ERROR"
+                persons[index].firstName = jsonPerson["surname"] as? String ?? "ERROR"
+                persons[index].avatarURL = jsonPerson["pictureUrl"] as? String
+            }else{
+                let person = Person(context: context)
+                person.lastName = jsonPerson["lastname"] as? String
+                person.firstName = jsonPerson["surname"] as? String
+                person.avatarURL = jsonPerson["pictureUrl"] as? String
+                person.id = Int32(id)
+            }
+        }
+        do{
+            if context.hasChanges{
+                try context.save()
+            }
+        }catch{
+            print(error)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func addOnServer(person: Person){
+        let urlString = "http://192.168.116.2:3000/persons"
+        
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        let parameters = ["firstname": person.firstName, "lastname": person.lastName, "pictureUrl": person.avatarURL]
+        //create the url with URL
+        let url = URL(string: urlString)!
+        //create the session object
+        let session = URLSession.shared
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
-    */
-
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 extension ContactsTableViewController: AddViewControllerDelegate{
@@ -169,7 +292,7 @@ extension ContactsTableViewController: AddViewControllerDelegate{
                 print(error.localizedDescription)
             }
             navigationController?.popViewController(animated: true)
-            tableView.reloadData()
+            //tableView.reloadData()
         }
     }
 }
@@ -179,7 +302,50 @@ extension ContactsTableViewController: DetailsViewControllerDelegate{
         context.delete(person)
         try? context.save()
         navigationController?.popViewController(animated: true)
+        //tableView.reloadData()
+    }
+}
+
+extension ContactsTableViewController: NSFetchedResultsControllerDelegate{
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if let frc = self.resultController {
+            return frc.sections!.count
+        }
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = self.resultController?.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sectionInfo = resultController?.sections?[section] else {
+            return nil
+        }
+        return sectionInfo.name
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return resultController?.sectionIndexTitles
+    }
+    
+    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        guard let result = resultController?.section(forSectionIndexTitle: title, at: index) else {
+            fatalError("Unable to locate section for \(title) at index: \(index)")
+        }
+        return result
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>){
         tableView.reloadData()
     }
+    
+    /*func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+     tableView.beginUpdates()
+     }*/
+    
 }
 
